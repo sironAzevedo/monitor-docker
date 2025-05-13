21b97786b2bc9eff8d1a2ae0179dba04101b0e3f
sironAzevedo <sirondba@gmail.com>
2025-05-13 02:23:22 -0300

    ajusteno icone

--- src/utils/constantes_utils.py
+++ src/utils/constantes_utils.py
@@ -3,7 +3,7 @@ from src.services.config_utils import resource_path, exist_file
 folder_dev = 'assets'
 folder_prod = '/opt/DockerMonitor/assets'
 
-ICON_PATH = f'{folder_prod}/docker_monitor.png' if exist_file(folder_prod) else resource_path(f"{folder_dev}/docker_monitor.png")
+ICON_PATH = f'{folder_prod}/app-icon.png' if exist_file(folder_prod) else resource_path(f"{folder_dev}/app-icon.png")
 ICON_HOME_PATH = f'{folder_prod}/home.png' if exist_file(folder_prod) else resource_path(f"{folder_dev}/home.png")
 ICON_REPORT_PATH = f'{folder_prod}/report.png' if exist_file(folder_prod) else resource_path(f"{folder_dev}/report.png")
 ICON_SETTINGS_PATH = f'{folder_prod}/settings.png' if exist_file(folder_prod) else resource_path(f"{folder_dev}/settings.png")
